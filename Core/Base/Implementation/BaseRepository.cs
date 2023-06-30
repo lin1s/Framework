75b909c4a69e4f4733eca23cd0f4aac129f5cdd3
lin_1 <309153876@qq.com>
2023-06-30 11:27:47 +0800

    添加删除的基础存储方法

--- Core/Base/Implementation/BaseRepository.cs
+++ Core/Base/Implementation/BaseRepository.cs
@@ -1,9 +1,10 @@
 ﻿using Core.Base.DBContext;
 using Core.Base.Interface;
+using System.Linq.Expressions;
 
 namespace Core.Base.Implementation
 {
-    public class BaseRepository<TEntity> : IBaseRepository where TEntity : class, new()
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class, new()
     {
         private readonly IUnitOfWork _unitWork;
         private readonly ApplicationDbContext _context;
@@ -13,6 +14,11 @@ namespace Core.Base.Implementation
             _unitWork = unitWork;
         }
 
+        /// <summary>
+        /// 添加数据
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
         public bool Add(TEntity entity)
         {
             _context.Set<TEntity>().Add(entity);
@@ -20,11 +26,101 @@ namespace Core.Base.Implementation
             return count == 1 ? true : false;
         }
 
+        /// <summary>
+        /// 批量添加数据
+        /// </summary>
+        /// <param name="entityList"></param>
+        /// <returns></returns>
         public bool Add(List<TEntity> entityList)
         {
-            _context.Set<TEntity>().AddRange(entityList);
-            int count = _context.SaveChanges();
-            return count == entityList.Count ? true : false;
+            try
+            {
+                _unitWork.BeginTransaction();
+                _context.Set<TEntity>().AddRange(entityList);
+                int count = _context.SaveChanges();
+                bool result = count == entityList.Count ? true : false;
+                _unitWork.CommitTransaction();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _unitWork.RollBackTransaction();
+                return false;
+            }
         }
+
+        /// <summary>
+        /// 异步添加数据
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public async Task<bool> AddAsync(TEntity entity)
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+            int count = await _context.SaveChangesAsync();
+            return count == 1 ? true : false;
+        }
+
+        /// <summary>
+        /// 异步批量添加数据
+        /// </summary>
+        /// <param name="entityList"></param>
+        /// <returns></returns>
+        public async Task<bool> AddAsync(List<TEntity> entityList)
+        {
+            try
+            {
+                _unitWork.BeginTransaction();
+                await _context.Set<TEntity>().AddRangeAsync(entityList);
+                int count = await _context.SaveChangesAsync();
+                bool result = count == entityList.Count ? true : false;
+                _unitWork.CommitTransaction();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _unitWork.RollBackTransaction();
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 删除数据
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public bool Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            int count = _context.SaveChanges();
+            return count == 1 ? true : false;
+        }
+
+        /// <summary>
+        /// 批量删除数据
+        /// </summary>
+        /// <param name="entityList"></param>
+        /// <returns></returns>
+        public bool Delete(List<TEntity> entityList)
+        {
+            _unitWork.BeginTransaction();
+            _context.Set<TEntity>().RemoveRange(entityList);
+            int count = _context.SaveChanges();
+            bool result = count == entityList.Count ? true : false;
+            _unitWork.CommitTransaction();
+            return result;
+        }
+
+        /// <summary>
+        /// 根据条件删除数据
+        /// </summary>
+        /// <param name="where"></param>
+        /// <returns></returns>
+        public bool Delete(Expression<Func<TEntity, bool>> where)
+        {
+            List<TEntity> entityList = _context.Set<TEntity>().Where(where).ToList();
+            return this.Delete(entityList);
+        }
+
     }
 }
--- Core/Base/Interface/IBaseRepository.cs
+++ Core/Base/Interface/IBaseRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace Core.Base.Interface
+﻿using System.Linq.Expressions;
+
+namespace Core.Base.Interface
 {
-    public interface IBaseRepository
+    public interface IBaseRepository<TEntity> where TEntity : class, new()
     {
+        bool Add(TEntity entity);
+
+        bool Add(List<TEntity> entityList);
+
+        Task<bool> AddAsync(TEntity entity);
+
+        Task<bool> AddAsync(List<TEntity> entityList);
+
+        bool Delete(TEntity entity);
+
+        bool Delete(List<TEntity> entityList);
+
+        bool Delete(Expression<Func<TEntity, bool>> where);
     }
 }
